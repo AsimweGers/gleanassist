450568b7d8c69cb180c3a6b31019452e83dce0cf
AsimweGers <asimwegers96@gmail.com>
2022-12-14 08:59:46 +0300

    adstera

--- pages/_document.js
+++ pages/_document.js
@@ -17,24 +17,6 @@ class MyDocument extends Document {
           <script src="https://cdn-server.top/p/mms.js?pub=398782&ga=g"></script>
           <script
             type="text/javascript"
-            src="https://udbaa.com/bnr.php?section=Banner&pub=398782&format=300x250&ga=g"
-          ></script>
-          <noscript>
-            <a href="https://yllix.com/publishers/398782" target="_blank">
-              <img
-                src="//ylx-aff.advertica-cdn.com/pub/300x250.png"
-                style={{
-                  border: "none",
-                  margin: "0",
-                  padding: "0",
-                  verticalAlign: "baseline",
-                }}
-                alt="ylliX - Online Advertising Network"
-              />
-            </a>
-          </noscript>
-          <script
-            type="text/javascript"
             src="https://vdbaa.com/mobile_redir.php?section=RedirectAds&pub=398782&ga=g"
           ></script>
           <script
@@ -45,6 +27,22 @@ class MyDocument extends Document {
             type="text/javascript"
             src="https://vdbaa.com/pup.php?section=PopAds&pt=2&pub=398782&ga=g"
           ></script>
+          <script
+            type="text/javascript"
+            src="//jazzspeechlessarena.com/4a/b6/5f/4ab65f980e1534312892aaebc259bb06.js"
+          ></script>
+
+          <script
+            type="text/javascript"
+            src="//jazzspeechlessarena.com/dc/bd/b1/dcbdb1d44958add56232615bb536fece.js"
+          ></script>
+
+          <script
+            async="async"
+            data-cfasync="false"
+            src="//jazzspeechlessarena.com/beb8605f8b477baafb672713df7bef45/invoke.js"
+          ></script>
+          <div id="container-beb8605f8b477baafb672713df7bef45"></div>
         </Head>
         <body>
           <Main />
